8fd150547c1be04844aa8d3d9ef787fde7353249
Theivanai Kumarappan <thkumara@c2r7s3.42singapore.sg>
2025-04-18 20:00:34 +0800

    Done

--- minishell/src/execution/signal.c
+++ minishell/src/execution/signal.c
@@ -6,7 +6,7 @@
 /*   By: thkumara <thkumara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 18:02:32 by thkumara          #+#    #+#             */
-/*   Updated: 2025/04/15 18:35:29 by thkumara         ###   ########.fr       */
+/*   Updated: 2025/04/15 18:54:21 by thkumara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,28 @@ void    set_signals(void)
     if (sigaction(SIGINT, &sa_sig, NULL) == -1)
         perror("sigaction");
 
+    signal(SIGQUIT, SIG_IGN);
+}
+
+void ignore_sigquit(void)
+{
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGQUIT, &sa, NULL);
+}
+
+void heredoc_sigint_handler(int sig)
+{
+    (void)sig;
+    write(1, "\n", 1);
+    exit(130);  // Bash exits heredoc on Ctrl+C with 130
+}
+
+void setup_heredoc_signals(void)
+{
+    signal(SIGINT, heredoc_sigint_handler);
     signal(SIGQUIT, SIG_IGN);
 }
 
